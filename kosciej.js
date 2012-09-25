8537f4b89800b77aa47c92d44b003bd46990baa7
Kasia <kasia@drzyzga.pl>
2012-09-26 01:50:13 +0200

    Added event delegation resources and began DOM manipulation excercise

---
+++ kosciej.js
@@ -0,0 +1,7 @@
+/*
+ * Opowieść o Kościeju jako przykłąd tworzenia i zagnieżdżania elementów w JSie
+ * przygotowany css - w htmlu początkowo tylko dusza
+ * stworzenie igły i dodanie jej do dokumentu - włożenie duszy do środka
+ * zadanie - ukryc duszę kościeja!
+ * zadanie - schowac w skrzyni krolika i kaczke i igle aw kaczce jajko w duszy
+ */
